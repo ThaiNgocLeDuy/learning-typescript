685d7e4684ba6ac24214cdcefc06377ad4a4be0c
ThaiNgocLeDuy <64624794+ThaiNgocLeDuy@users.noreply.github.com>
2021-08-14 16:13:10 +0700

    lession 02

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-var char = "mario";
-console.log(char);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-  console.log(input);
-});
+var str = "Hello Typescript";
+console.log(str);
+//Type 'number' is not assignable to type 'string'
+// str = 12;
+str = "Hi";
+console.log(str);
+var num = 100;
+console.log(num);
+//Type 'string' is not assignable to type 'number'.
+// num = "Hello";
+num = 101;
+console.log(num);
+var circle = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circle("Hello")); // output NaN
+var circle2 = function (diameter) {
+    return diameter * Math.PI;
+};
+//Argument of type 'string' is not assignable to parameter of type 'number'
+// console.log(circle2("Hello"))
+console.log(circle2(10));
--- app.ts
+++ app.ts
@@ -1,11 +1,31 @@
-let char = "mario";
+let str = "Hello Typescript";
 
-console.log(char);
+console.log(str);
 
-const inputs = document.querySelectorAll("input");
+//Type 'number' is not assignable to type 'string'
+// str = 12;
+str = "Hi";
+console.log(str);
 
-console.log(inputs);
+let num = 100;
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+console.log(num);
+
+//Type 'string' is not assignable to type 'number'.
+// num = "Hello";
+
+num = 101;
+console.log(num);
+
+const circle = (diameter) => {
+  return diameter * Math.PI;
+};
+
+console.log(circle("Hello")); // output NaN
+
+const circle2 = (diameter: number) => {
+  return diameter * Math.PI;
+};
+//Argument of type 'string' is not assignable to parameter of type 'number'
+// console.log(circle2("Hello"))
+console.log(circle2(10));
